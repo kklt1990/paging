09a04665cbcf99e6b3c08e15c03572c14d8ce87d
kklt <18332763730@163.com>
2017-04-20 22:30:01 +0800

    创建student实体类，page实体类。创建paging数据库，student数据表。

---
+++ src/main/java/com/kklt/paging/entity/Page.java
@@ -0,0 +1,92 @@
+package com.kklt.paging.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by root on 17-4-20.
+ */
+public class Page<T> implements Serializable {
+    /**
+     * pageSize 每一页有多少条数据
+     */
+    private Integer pageSize;
+    /**
+     * currentPageNumber 当前页的页码
+     */
+    private Integer currentPageNumber;
+    /**
+     * totalSize 一共有多少条数据
+     */
+    private Integer  totalRecordSize;
+    /**
+     * totalPageSize 一共有多少页
+     */
+    private Integer totalPageSize;
+    /**
+     * currentPageRecord 当前页的记录集合
+     */
+    private List<T> currentPageRecord;
+
+    /**
+     * 无参的构造方法
+     */
+    public Page() {
+    }
+
+    /**
+     * 所有参数的构造方法
+     * @param pageSize
+     * @param currentPageNumber
+     * @param totalRecordSize
+     * @param totalPageSize
+     * @param currentPageRecord
+     */
+    public Page(Integer pageSize, Integer currentPageNumber, Integer totalRecordSize, Integer totalPageSize, List<T> currentPageRecord) {
+        this.pageSize = pageSize;
+        this.currentPageNumber = currentPageNumber;
+        this.totalRecordSize = totalRecordSize;
+        this.totalPageSize = totalPageSize;
+        this.currentPageRecord = currentPageRecord;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Integer getCurrentPageNumber() {
+        return currentPageNumber;
+    }
+
+    public void setCurrentPageNumber(Integer currentPageNumber) {
+        this.currentPageNumber = currentPageNumber;
+    }
+
+    public Integer getTotalRecordSize() {
+        return totalRecordSize;
+    }
+
+    public void setTotalRecordSize(Integer totalRecordSize) {
+        this.totalRecordSize = totalRecordSize;
+    }
+
+    public Integer getTotalPageSize() {
+        return totalPageSize;
+    }
+
+    public void setTotalPageSize(Integer totalPageSize) {
+        this.totalPageSize = totalPageSize;
+    }
+
+    public List<T> getCurrentPageRecord() {
+        return currentPageRecord;
+    }
+
+    public void setCurrentPageRecord(List<T> currentPageRecord) {
+        this.currentPageRecord = currentPageRecord;
+    }
+}
---
+++ src/main/java/com/kklt/paging/entity/Student.java
@@ -0,0 +1,91 @@
+package com.kklt.paging.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by root on 17-4-20.
+ */
+public class Student implements Serializable{
+    /**
+     * id 学生的id
+     */
+    private Integer id;
+    /**
+     * name 学生的姓名
+     */
+    private String name;
+    /**
+     * age 学生的年龄
+     */
+    private Byte age;
+    /**
+     * sex 学生的性别
+     */
+    private Integer sex;
+    /**
+     * address 学生的地址
+     */
+    private String address;
+
+    /**
+     * 空的构造方法
+     */
+    public Student() {
+    }
+
+    /**
+     * 所有参数的构造方法
+     * @param id
+     * @param name
+     * @param age
+     * @param sex
+     * @param address
+     */
+    public Student(Integer id, String name, Byte age, Integer sex, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Byte getAge() {
+        return age;
+    }
+
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
